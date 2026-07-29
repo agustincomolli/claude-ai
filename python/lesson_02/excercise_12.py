a6e405c88f36261550314c428b962c1517b66e1c
Agustín Comolli <agustincomolli@yahoo.com.ar>
2026-07-29 16:28:37 -0300

    add excercise 12

--- python/lesson_02/excercise_12.py
+++ python/lesson_02/excercise_12.py
@@ -10,3 +10,27 @@ sobre strings) Y es empresa → prioridad "CRÍTICA"
 - Si no contiene "servidor" pero es empresa → prioridad "MEDIA"
 - En cualquier otro caso → prioridad "BAJA"
 """
+
+print("=== Clasificador de tickets de soporte por prioridad ===")
+
+problem_type = input("Tipo de problema: ")
+client = input("¿Empresa o particular? ")
+IS_ENTERPRISE = client.lower() == "empresa"
+KEYWORD = "servidor"
+
+if KEYWORD in problem_type and IS_ENTERPRISE:
+    PRIORITY = "CRITICA"
+elif KEYWORD in problem_type:
+    PRIORITY = "ALTA"
+elif IS_ENTERPRISE:
+    PRIORITY = "MEDIA"
+else:
+    PRIORITY = "BAJA"
+
+print(
+    "\n+-----------------------------+"
+    "\n+ La prioridad del ticket es: +"
+    "\n+-----------------------------+"
+    f"\n             {PRIORITY}"
+    "\n+-----------------------------+"
+    )
