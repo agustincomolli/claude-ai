e5984a3e95325d6f98575c8542503ceb17c0b3f0
Agustín Comolli <agustincomolli@gmail.com>
2026-08-24 13:41:23 -0300

    feat: print all repairs

--- python/projects/project_01/gtrsys.py
+++ python/projects/project_01/gtrsys.py
@@ -1,17 +1,31 @@
 """
 Sistema de Gestión de Tickets de Reparación
 
-Versión en consola, simplificada, de un sistema con menú interactivo para 
-cargar, consultar y gestionar tickets de reparación de equipos. 
+Versión en consola, simplificada, de un sistema con menú interactivo para
+cargar, consultar y gestionar tickets de reparación de equipos.
 """
 
+TOTAL_WIDTH = 80
+MARGIN_LEFT = 12
 
-def input_choice(margin_left, menu_length):
+
+def print_header():
+    """
+    Imprime el encabezado de la aplicación
+    """
+    title = "📋 === SISTEMA DE GESTIÓN DE REPARACIONES === [x]"
+
+    # Imprimir el encabezado.
+    print(f"\n+{'-' * (TOTAL_WIDTH - 2)}+")
+    print(f"{title:^{TOTAL_WIDTH}}")
+    print(f"+{'-' * (TOTAL_WIDTH - 2)}+\n")
+
+
+def input_choice(menu_length):
     """
     Valida que el usuario ingrese un número entre las opciones de menú.
-    
+
     Args:
-        margin_left: margen izquierdo para empezar a imprimir mensajes.
         menu_lenght: cantidad de opciones que hay en el menú.
 
     Returns:
@@ -20,14 +34,14 @@ def input_choice(margin_left, menu_length):
     while True:
         try:
             menu_selected = int(
-                input(f"\n{' ' * margin_left}Seleccione una opción: "))
+                input(f"\n{' ' * MARGIN_LEFT}Seleccione una opción: "))
             if menu_selected <= 0 or menu_selected > menu_length:
                 raise ValueError
             return menu_selected
         except ValueError:
             error_message = "❌ ERROR: Debe ingresar una opción válida del 1 "
             error_message += f"al {menu_length}."
-            print(f"{' ' * margin_left}{error_message}")
+            print(f"{' ' * MARGIN_LEFT}{error_message}")
 
 
 def show_menu():
@@ -38,7 +52,7 @@ def show_menu():
         El número de menú elegido por el usuario.
     """
     menu_list = (
-        "Cargar nueva reparació",
+        "Cargar nueva reparación",
         "Listar todas las reparaciones",
         "Buscar reparaciones por cliente",
         "Marcar reparación como entregada",
@@ -46,20 +60,14 @@ def show_menu():
         "Salir"
     )
 
-    total_width = 80
-    title = "📋 === SISTEMA DE GESTIÓN DE REPARACIONES === [x]"
-    margin_left = 12
-
-    # Imprimir el encabezado.
-    print(f"\n+{'-' * (total_width - 2)}+")
-    print(f"{title:^{total_width}}")
-    print(f"+{'-' * (total_width - 2)}+\n")
+    print_header()
+    print(f"{' ' * MARGIN_LEFT}MENU PRINCIPAL\n")
 
     # Opciones del menú
     for i, menu_item in enumerate(menu_list):
-        print(f"{' ' * margin_left}{i+1}. {menu_item}")
+        print(f"{' ' * MARGIN_LEFT}{i+1}. {menu_item}")
 
-    return input_choice(margin_left, len(menu_list))
+    return input_choice(len(menu_list))
 
 
 def input_cost(message):
@@ -89,6 +97,10 @@ def new_repair():
     Returns:
         Diccionario que contiene los datos de la reparación.
     """
+
+    print_header()
+    print("NUEVA REPARACIÓN\n")
+
     name = input("Nombre: ")
     equipment = input("Equipo [marca/model]: ")
     description = input("Descripción: ")
@@ -104,6 +116,36 @@ def new_repair():
     }
 
 
+def press_enter_to_continue():
+    """
+    Genera una pausa en el programa hasta que el usuario presiona ENTER
+    """
+
+    input("\nPresione ENTER para continuar...")
+
+
+def print_repairs():
+    """
+    Imprime una tabla con las reparaciones cargadas.
+    """
+    # Especificar anchos de columnas para la tabla.
+    w1, w2, w3, w4, w5, sign = 4, 20, 25, 11, 15, 2
+
+    print_header()
+    print(f"{'LISTADO DE REPARACIONES':^{TOTAL_WIDTH}}\n")
+    print(f"{'ID':>{w1}} {'CLIENTE':<{w2}} {'EQUIPO':<{w3}} "
+          f"{'COSTO':>{w4 + sign}} {'ESTADO':<{w5}}")
+
+    for reparation_id, reparation in reparations.items():
+        row = f"{reparation_id:>{w1}} {reparation['nombre']:<{w2}} "
+        row += f"{reparation['equipo']:<{w3}} "
+        row += f"$ {reparation['costo estimado']:>{w4}.2f} "
+        row += f"{reparation['estado']:<{w5}}"
+        print(row)
+
+    press_enter_to_continue()
+
+
 # Maneja el ID autoincremental.
 last_id = 0
 reparations = {}
@@ -115,7 +157,7 @@ while True:
         reparations[str(new_id)] = new_repair()
         last_id = new_id
     if user_choice == 2:
-        pass
+        print_repairs()
     if user_choice == 3:
         pass
     if user_choice == 4:
@@ -125,6 +167,3 @@ while True:
     if user_choice == 6:
         print("\n¡Hasta pronto! 👋")
         break
-    print(f"Eligió: {user_choice}")
-    print(f"\n{reparations}")
-    input()
